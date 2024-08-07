0f7c2e5e25ae03e95609539e74bb91108928c9f7
KD124 <s3836280@rmit.edu.vn>
2024-08-08 04:06:23 +0700

    add favorites screen

--- src/screens/Favorites.tsx
+++ src/screens/Favorites.tsx
@@ -1,12 +1,37 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import React, { useMemo } from "react";
+import { useNavigationSearch } from "../hooks/useNavigationSearch";
+import { Track } from "react-native-track-player";
+import TrackList from "../components/TrackList";
+import { tabBarHeight } from "../utils/constants";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { tracks } from "../../assets/data/library";
 
 export default function Favorites() {
+  const insets = useSafeAreaInsets();
+  const search = useNavigationSearch({ placeholder: "Find in songs" });
+  const favoriteTracks = useMemo(() => {
+    return tracks.filter((track: Track) => track.rating === 1);
+  }, []);
+
+  // const filteredTracks = useMemo(() => {
+  //   if (!search) return tracks;
+  //   return tracks.filter((song: Track) =>
+  //     song.title?.toLowerCase().includes(search.toLowerCase().trim())
+  //   );
+  // }, [search]);
   return (
-    <View>
-      <Text>Favorites</Text>
+    <View style={styles.container}>
+      <ScrollView
+        style={{ marginBottom: tabBarHeight + insets.bottom + 8 }}
+        contentInsetAdjustmentBehavior="automatic"
+      >
+        <TrackList tracks={favoriteTracks} scrollEnabled={false} />
+      </ScrollView>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+});
